b51e048cb1a49703be41b2a98ee502d3ea9e73fb
Akshay1114 <sharma41114@gmail.com>
2025-02-07 18:57:15 -0800

    UPDATE : controller for save sleep data

--- controllers/sleepController.js
+++ controllers/sleepController.js
@@ -17,8 +17,9 @@ const { USER_ADDED, FETCH_USERS, UPDATE_USER, ALREADY_REGISTER, FETCH_USER, DELE
 const { RECORD_CREATED, RECORD_ALREADY_EXISTS, SUCCESS, BAD_REQUEST } = statusCodes;
 
 router.post('/saveSleepData', async(req, res) => {
-    console.log("saveSleepData FROM MOBIEL APP")
+    console.log("saveSleepData FROM MOBIEL APP SAVE TO DB")
     console.log('req.body', req.body)
+    
     // const { mobile } = req.body;
     
     // addUser(req.body)
