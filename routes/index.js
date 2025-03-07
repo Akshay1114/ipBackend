e7598cbdcc1c1cafc2e2f928d87c9389639ecdcb
Akshay1114 <sharma41114@gmail.com>
2025-03-07 01:51:37 -0800

    fea: notifications with socket io

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import { userController } from '../controllers/index.js';
 import { sleepController } from '../controllers/sleepController.js';
 import { scheduleController } from '../controllers/scheduleController.js';
 import { verifyToken } from '../middleware/verfifyToke.js';
+import { notificationController } from '../controllers/notificationController.js';
 // import { verifyToken} from './middleware/verifyToken.js'
 
 const router = Router();
@@ -12,5 +13,6 @@ router.use('/user', userController);
 router.use('/sleepData', sleepController);
 // router.use('/schedule',verifyToken, scheduleController)
 router.use('/schedule', scheduleController)
-
+// router.use('/v1', verifyToken)
+router.use('/getNotification', notificationController)
 export { router };
